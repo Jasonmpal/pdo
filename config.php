5b19943a580d6e33c444a9d8547657e3afd0e299
Jasonmpal <jasonmpal@knights.ucf.edu>
2018-08-25 17:36:38 -0400

    Update config.php

--- config.php
+++ config.php
@@ -5,10 +5,10 @@
  *
  */
 
-$host       = "localhost";
-$username   = "root";
-$password   = "root";
-$dbname     = "test";
+$host       = "aa16glunvbqnu58.ctogpmoawefq.us-east-1.rds.amazonaws.com";
+$username   = "JPalatucci";
+$password   = "PolySly87";
+$dbname     = "testDB";
 $dsn        = "mysql:host=$host;dbname=$dbname";
 $options    = array(
                 PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
