a21d94db3b430981647ca9dd3efdc2daee11295a
Jasonmpal <jasonmpal@knights.ucf.edu>
2018-08-25 17:38:50 -0400

    Update config.php

--- config.php
+++ config.php
@@ -8,7 +8,7 @@
 $host       = "aa16glunvbqnu58.ctogpmoawefq.us-east-1.rds.amazonaws.com";
 $username   = "JPalatucci";
 $password   = "PolySly87";
-$dbname     = "testDB";
+$dbname     = "DemoTEST";
 $dsn        = "mysql:host=$host;dbname=$dbname";
 $options    = array(
                 PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
